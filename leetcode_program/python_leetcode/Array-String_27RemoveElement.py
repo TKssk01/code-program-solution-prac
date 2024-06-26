36fb2326f87d47131e9d7a20965ac4d9e151bdee
Takahiro <149182031+TKssk01@users.noreply.github.com>
2024-06-26 06:23:29 -0400

    20240626_PracticeCode_3

--- leetcode_program/python_leetcode/Array-String_27RemoveElement.py
+++ leetcode_program/python_leetcode/Array-String_27RemoveElement.py
@@ -2,7 +2,10 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        # ループを開始。'nums'リストに'val'が含まれている間はループを続ける。
         while val in nums:
+            # 'nums'リストから最初に見つかる'val'の値を削除する。
             nums.remove(val)
-            return len(nums)
+        # ループが終了したら、'nums'リストの長さを返す。
+        return nums
     
