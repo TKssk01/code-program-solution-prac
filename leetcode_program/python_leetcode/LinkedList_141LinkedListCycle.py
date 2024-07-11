2a90346b3459232a6beac2e4ced51af7ceb98c03
Takahiro <149182031+TKssk01@users.noreply.github.com>
2024-07-11 16:38:42 -0400

    20240711_CodePractice_2

--- leetcode_program/python_leetcode/LinkedList_141LinkedListCycle.py
+++ leetcode_program/python_leetcode/LinkedList_141LinkedListCycle.py
@@ -2,10 +2,8 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if not head or not head.next:
             return False
-
         slow = head
         fast = head.next
-
         while slow != fast:
             if not fast or not fast.next:
                 return False
