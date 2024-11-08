3f6ee05d7d9ba0b3de2a852a0c11c379ef489f46
TKssk01 <takahiro.sasaki.j@gmail.com>
2024-11-08 13:51:29 -0500

    20241108

---
+++ p_prac/D314.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int Sum = n * m;
+        
+        System.out.println(Sum);
+    }
+}
